62a629397e416e48d696aca78b5564ecfa72b554
aiconsultantteam <139223990+aiconsultantteam@users.noreply.github.com>
2023-07-17 14:03:33 +0700

    Update settings.py

--- email_webapp/settings.py
+++ email_webapp/settings.py
@@ -110,5 +110,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://email-django-app.onrender.com','https://aiconsultant.org']
+CSRF_TRUSTED_ORIGINS = ['https://email-django-app.onrender.com','https://aiconsultant.org/email-marketing-ai-powered']
 
