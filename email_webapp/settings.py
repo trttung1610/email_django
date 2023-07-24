a4e930234b34b57f5f72f434d4d21785a20de1cf
trttung1610 <94973318+trttung1610@users.noreply.github.com>
2023-07-25 02:55:38 +0700

    Update settings.py

--- email_webapp/settings.py
+++ email_webapp/settings.py
@@ -27,14 +27,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Place the CorsMiddleware before CsrfViewMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 
 CORS_ALLOWED_ORIGINS = [
     "https://email-sender-m5fi.onrender.com",
